3630bb479191dc3ced872d17402295aae094c095
Amos Wachanga <wachanga.amos@gmail.com>
2016-02-18 13:39:20 +0300

    Update GreenFly.java

--- GreenFly.java
+++ GreenFly.java
@@ -17,8 +17,8 @@ Write a Java program to solve the following problem.
 
 Greenfly can reproduce asexually. After one week of life a lone female can produce eight offspring a day. 
 Starting at the beginning of day 1 with a single mature female, how many greenfly could there be by the end of day 28? It may be assumed that:
-	There are no deaths
-	All offspring are females
+:There are no deaths
+:All offspring are females
 Note that at the end of day 1 there will be 9 greenfly (original + 8 offspring). 
 At the end of day 7 there will be 57 greenfly (original + 8 × 7 offspring). 
 At the end of day 8 there will be 129 greenfly (original + 8 × 8 offspring + 64 offspring from the daughters produced on day 1).
@@ -46,11 +46,8 @@ At the end of day 8 there will be 129 greenfly (original + 8 × 8 offspring + 64
         }else{            
             int numberOfDaysForTheWeek = 7*numberOfWeeks;   
             
-            //System.out.println("Number of Days For the Week "+numberOfDaysForTheWeek);
-            
             int v = (int) (Math.pow(8, numberOfWeeks) * (day-numberOfDaysForTheWeek)); 
             
-            //System.out.println("v "+v);
                         
             return (calculateParents(numberOfDaysForTheWeek)+ v);
         }
